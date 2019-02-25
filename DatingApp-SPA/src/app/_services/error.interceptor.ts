e0937c30eaac14986dfa13bfacd4c8213013194c
RoyZaply <rosario1711@gmail.com>
2019-02-25 17:26:37 +0100

    End of section 5-rev.2

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -4,30 +4,32 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
-export class ErrorInterceptor implements HttpInterceptor{
+export class ErrorInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
             catchError(error => {
                 if (error instanceof HttpErrorResponse) {
-                    if (error.status===401) {
+                    if (error.status === 401) {
                         return throwError(error.statusText);
                     }
                     const applicationError = error.headers.get('Application-Error');
                     if (applicationError) {
-                        console.log(applicationError);
+                        console.error(applicationError);
                         return throwError(applicationError);
-                    } 
+                    }
                     const serverError = error.error;
                     let modalStateErrors = '';
-                    if ( serverError && typeof serverError === 'object') {
-                        for ( const key in serverError )
+                    if ( serverError && typeof serverError === 'object') 
+                    {
+                        //console.log( JSON.stringify(serverError) );
+                        for ( const key in serverError.errors )
                         {
-                            if (serverError[key]) {
-                                modalStateErrors += serverError[key] + '\n';
+                            if (serverError.errors[key]) {
+                                modalStateErrors += serverError.errors[key] + '\n';
                             }
                         }
                     }
-                    return throwError(modalStateErrors || serverError || 'Server Error');
+                    return throwError( modalStateErrors || serverError || 'Server Error');
                 }
             })
         );
