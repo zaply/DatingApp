bd983cc2d299e4df4e0069565d27904e81513d33
RoyZaply <rosario1711@gmail.com>
2019-03-05 12:52:32 +0100

    End of section 9

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user'; 
+
+/*
+ * remove httpOptions because we introduced in app.module.ts the JwtModule
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization' : 'Bearer ' + localStorage.getItem('token')
+  })
+};
+
+* return this.http.get<User>(this.baseUrl + 'user/' + id, httpOptions);
+*/
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseUrl = environment.apiUrl;
+
+constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users/');
+  }
+
+  getUser(id): Observable<User> {
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
+}
